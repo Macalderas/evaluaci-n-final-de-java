e992b634c93997925acdce34b660aa0388fe9499
Macalderas <159477202+Macalderas@users.noreply.github.com>
2024-10-07 10:09:22 -0600

    Update TareaView.java

--- Gestion_de_tareas/src/view/TareaView.java
+++ Gestion_de_tareas/src/view/TareaView.java
@@ -1,4 +1,19 @@
 package view;
 
-public class TareaView {
+import javax.swing.*;
+import java.awt.*;
+
+public class TareaView  extends JPanel {
+    public TareaView(String titulo, String descripcion, String fechaLimite, String estado) {
+        setLayout(new GridLayout(1, 4, 10, 10));
+        JLabel lblTitulo = new JLabel(titulo);
+        JLabel lblDescripcion = new JLabel(descripcion);
+        JLabel lblFechaLimite = new JLabel(fechaLimite);
+        JLabel lblEstado = new JLabel(estado);
+
+        add(lblTitulo);
+        add(lblDescripcion);
+        add(lblFechaLimite);
+        add(lblEstado);
+    }
 }
